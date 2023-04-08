b7ce804d095f11be477bac22187613992e272b53
lukmanps <luqmansha6@gmail.com>
2023-04-08 10:25:27 +0530

    Error fixed

--- scribbles.js
+++
@@ -1,4 +0,0 @@
-<li class="nav-item">
-    <a class="nav-link active" id="register-tab-2" data-toggle="tab" href="/signup"
-        role="tab" aria-controls="register-2" aria-selected="false">Register</a>
-</li>
